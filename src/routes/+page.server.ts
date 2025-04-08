f5282f161e7b00605faa8b3e2713922397d2c198
tropicbliss <tropicbliss@protonmail.com>
2025-04-09 00:04:45 +0800

    E

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,6 @@
 import { trpcServer } from "$lib/server/server";
 
-export const load = async (event) => {
+export const load = async () => {
     // You don't need to return the result of this function,
     // just call it and your data will be hydrated!
     await trpcServer.greeting.ssr({ name: "the e7 stack" });
